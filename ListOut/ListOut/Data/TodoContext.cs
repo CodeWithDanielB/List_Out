2819926cb43e5e425fadab9537c06c2e009cacef
CodeWithDanielB <danielbiswakarma24@gmail.com>
2024-03-13 10:11:04 +0545

    completed the frontend

---
+++ ListOut/ListOut/Data/TodoContext.cs
@@ -0,0 +1,15 @@
+﻿using ListOut.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ListOut.Data
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
---
+++ ListOut/ListOut/Models/TodoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ListOut.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool isCompleted { get; set; }
+
+    }
+}
